12e93983bfba017e0705513865e048033309b8cd
lov3five <luongtranstaff@gmail.com>
2022-09-15 13:02:53 +0700

    update

---
+++ LAB2_2_15_09_2022_FILECODE/audio_dsp.py
@@ -0,0 +1,22 @@
+""" 
+Họ tên: Trần Thanh Lượng
+MSSV: 19525521
+"""
+
+import numpy as np
+import wave
+import struct
+import matplotlib.pyplot as plt
+
+# frequency là tần số
+frequency = 1000
+
+num_samples = 48000
+
+# sampling_rate là tốc độ lấy mẫu
+sampling_rate = 48000.0
+
+# amplitude là biên độ
+amplitude = 16000
+
+file = "test.wav"
