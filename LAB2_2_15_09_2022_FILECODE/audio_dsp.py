63db26477cad1a0b76e4fbf432201a7457ade0bf
lov3five <luongtranstaff@gmail.com>
2022-09-15 13:09:53 +0700

    uodate

--- LAB2_2_15_09_2022_FILECODE/audio_dsp.py
+++ LAB2_2_15_09_2022_FILECODE/audio_dsp.py
@@ -8,15 +8,34 @@ import wave
 import struct
 import matplotlib.pyplot as plt
 
+import numpy as np
+import wave
+# struct là 1 thư viện của python lấy dữ liệu và đóng gói dưới dạng dữ liệu binary
+import struct
+import matplotlib.pyplot as plt
+
 # frequency là tần số
 frequency = 1000
-
+# num_samples là số lượng mẫu
 num_samples = 48000
-
-# sampling_rate là tốc độ lấy mẫu
+# sampling_rate là tốc độ lấy mẫu của chuyển đổi từ tương tự sang kỹ thuật số
 sampling_rate = 48000.0
-
-# amplitude là biên độ
+# amplitutde là biên độ
 amplitude = 16000
-
+# file chứa định dạng âm thanh
 file = "test.wav"
+# sine_wave là hàm sin
+sine_wave = [np.sin(2 * np.pi * frequency * x/sampling_rate) for x in range(num_samples)]
+
+
+# nframes: số lượng khung hình
+# comptype 
+# compname
+# nchannels: số kênh
+# sampwidth: chiều rộng mẫu (byte)
+
+nframes=num_samples
+comptype="NONE"
+compname="not compressed"
+nchannels=1
+sampwidth=2
